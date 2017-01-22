024774926a9f33e0207e3fee478dd15add830448
Robert Dale <robdale@gmail.com>
2017-01-22 17:00:46 -0500

    minor fix

--- keyboards/atredox/keymaps/default/keymap.c
+++ keyboards/atredox/keymaps/default/keymap.c
@@ -35,7 +35,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_QWERTY] = {
   {KC_EQL,  KC_1,   KC_2,    KC_3,    KC_4,     KC_5,    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_6,   KC_7,    KC_8,    KC_9,    KC_0,    KC_MINUS},
-  {KC_TAB,  KC_Q,   KC_W,    KC_E,    KC_R,     KC_T,    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_Y,   KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL},
+  {KC_TAB,  KC_Q,   KC_W,    KC_E,    KC_R,     KC_T,    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_Y,   KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS},
   {KC_LCTL, KC_A,   KC_S,    KC_D,    KC_F,     KC_G,    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_H,   KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT},
   {KC_LSFT, KC_Z,   KC_X,    KC_C,    KC_V,     KC_B,    KC_LCTL, KC_LALT, KC_RALT, KC_RCTL, KC_N,   KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT },
   {RAISE,   KC_GRV, KC_BSLS, KC_LEFT, KC_RIGHT, KC_BSPC, KC_DEL,  KC_NO,   KC_NO,   KC_ENT,  KC_SPC, KC_DOWN, KC_UP,   KC_LBRC, KC_RBRC, RAISE}
